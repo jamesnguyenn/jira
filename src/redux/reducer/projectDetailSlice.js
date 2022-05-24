db171345ac468a37299fb6bb74d1801050a7d5ca
Khoa Hoang <hoangkhoa1998@gmail.com>
2022-05-24 22:01:31 +0700

    ProjectMangeV2.1_ModalAdjust

--- src/redux/reducer/projectDetailSlice.js
+++ src/redux/reducer/projectDetailSlice.js
@@ -1,41 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    data: {},
-    isLoading: false,
+  data: {},
+  isLoading: false,
 };
 
 const projectDetailReducer = createSlice({
-    name: 'projectDetail',
-    initialState,
-    reducers: {
-        getProjectDetailRequest: (state, action) => {
-            return {
-                ...state,
-                isLoading: true,
-            };
-        },
-        getProjectDetailSuccess: (state, action) => {
-            const { payload } = action;
-            return {
-                ...state,
-                data: { ...payload },
-                isLoading: false,
-            };
-        },
-        getProjectDetailFailure: (state, action) => {
-            return {
-                ...state,
-                isLoading: false,
-            };
-        },
+  name: 'projectDetail',
+  initialState,
+  reducers: {
+    getProjectDetailRequest: (state, action) => {
+      return {
+        ...state,
+        isLoading: true,
+      };
     },
+    getProjectDetailSuccess: (state, action) => {
+      const { payload } = action;
+      return {
+        ...state,
+        data: { ...payload },
+        isLoading: false,
+      };
+    },
+    getProjectDetailFailure: (state, action) => {
+      return {
+        ...state,
+        isLoading: false,
+      };
+    },
+  },
 });
 
 export const {
-    getProjectDetailRequest,
-    getProjectDetailSuccess,
-    getProjectDetailFailure,
+  getProjectDetailRequest,
+  getProjectDetailSuccess,
+  getProjectDetailFailure,
 } = projectDetailReducer.actions;
 
 export default projectDetailReducer.reducer;
--- src/redux/store/configStore.js
+++ src/redux/store/configStore.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducers from '../reducer/userSlice';
 import projectReducer from '../reducer/projectSlice';
 import projectDetailReducer from '../reducer/projectDetailSlice';
+import modalReducer from '../reducer/modalAdjustSlice';
 
 export const store = configureStore({
-    reducer: {
-        user: userReducers,
-        project: projectReducer,
-        projectDetail: projectDetailReducer,
-    },
+  reducer: {
+    user: userReducers,
+    project: projectReducer,
+    projectDetail: projectDetailReducer,
+    modalAdjust: modalReducer,
+  },
 });
