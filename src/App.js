d1a409cb80e5dbf5d6da734a0cbcbbed97f4814a
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2022-05-25 00:25:40 +0700

    Add Layout Modal & Add FormCreateEditProject

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ProtectRoute from './page/ProtectRoute';
 import Auth from './page/Auth';
 import HomeScreen from './page/HomeScreen';
 import LayoutMain from './layout/LayoutMain';
-import CreateProject from './page/CreateProject';
+import CreateProject from './component/FormCreateEditProject';
 import UserManagement from './page/UserManagement';
 import NotFoundPage from './page/NotFoundPage';
 import ProjectDetail from './page/ProjectDetail';
@@ -18,59 +18,57 @@ import { ACCESSTOKEN } from './axios';
 import { checkTokenThunk } from './redux/thunk';
 import { getUserInfo } from './redux/selectors';
 import { checkTokenRequest } from './redux/reducer/userSlice';
-import ModalAdjust from './component/_HOC/ModalAdjust';
+import LayoutModal from './layout/LayoutModal/LayoutModal';
 
 function App() {
-  const dispatch = useDispatch();
-  const { isCheckToken } = useSelector(getUserInfo);
+    const dispatch = useDispatch();
+    const { isCheckToken } = useSelector(getUserInfo);
 
-  useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem(ACCESSTOKEN));
-    if (userData) {
-      dispatch(checkTokenRequest());
-      const checkToken = checkTokenThunk(userData);
-      dispatch(checkToken);
-    }
-  }, [dispatch]);
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem(ACCESSTOKEN));
+        if (userData) {
+            dispatch(checkTokenRequest());
+            const checkToken = checkTokenThunk(userData);
+            dispatch(checkToken);
+        }
+    }, [dispatch]);
 
-  return (
-    <div className="App">
-      {isCheckToken ? (
-        <div className="view-center">
-          <Loading color="#000"></Loading>
+    return (
+        <div className="App">
+            {isCheckToken ? (
+                <div className="view-center">
+                    <Loading color="#000"></Loading>
+                </div>
+            ) : (
+                <Routes>
+                    <Route path="/auth" element={<Auth />} />
+                    <Route element={<ProtectRoute />}>
+                        <Route element={<LayoutMain />}>
+                            {/* Screens */}
+                            <Route path="/" element={<HomeScreen />} />
+                            <Route path="/adjust" element={<LayoutModal />} />
+                            <Route
+                                path="/create-project"
+                                element={<CreateProject />}
+                            />
+                            <Route
+                                path="/user-management"
+                                element={<UserManagement />}
+                            />
+                            {/* Detail Screens */}
+                            <Route
+                                path="/project-detail/:id"
+                                element={<ProjectDetail />}
+                            />
+                        </Route>
+                    </Route>
+                    {/* Not Found Page */}
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            )}
+            <ToastContainer />
         </div>
-      ) : (
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route element={<ProtectRoute />}>
-            <Route element={<LayoutMain />}>
-              {/* Screens */}
-              <Route path="/" element={<HomeScreen />} />
-              <Route path="/adjust" element={<ModalAdjust />} />
-              <Route
-                path="/create-project"
-                element={<CreateProject />}
-              />
-              <Route
-                path="/user-management"
-                element={<UserManagement />}
-              />
-              {/* Detail Screens */}
-              <Route
-                path="/project-detail/:id"
-                element={<ProjectDetail />}
-              />
-            </Route>
-          </Route>
-          {/* Not Found Page */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      )}
-      <ModalAdjust />
-
-      <ToastContainer />
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/layout/LayoutModal/LayoutModal.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import {
+    Button,
+    Col,
+    DatePicker,
+    Drawer,
+    Form,
+    Input,
+    Row,
+    Select,
+    Space,
+} from 'antd';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    closeModal,
+    openModal,
+    getProjectDetailModal,
+} from '../../redux/reducer/modalAdjustSlice';
+import ReactQuill from 'react-quill';
+import { getProjectDetail } from '../../redux/selectors';
+import { getProjectDetailThunk } from '../../redux/thunk';
+const { Option } = Select;
+
+export default function LayoutModal({ children }) {
+    const { visible } = useSelector((state) => state.modalAdjust);
+    const { data } = useSelector(getProjectDetail);
+    console.log('data', data);
+    const dispatch = useDispatch();
+    console.log('visible', visible);
+
+    const showDrawer = () => {
+        dispatch(openModal());
+    };
+
+    const onClose = () => {
+        dispatch(closeModal());
+    };
+
+    return (
+        <>
+            <Drawer
+                onClose={onClose}
+                width={720}
+                visible={visible} //Visible get from store will set in here
+                bodyStyle={{
+                    paddingBottom: 80,
+                }}
+            >
+                {children}
+            </Drawer>
+        </>
+    );
+}
